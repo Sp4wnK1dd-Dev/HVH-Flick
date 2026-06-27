06d4d74431b8789edeb6b7344a774f09f91442ab
Jason DEV <yarkisrngr@gmail.com>
2026-06-27 17:41:06 +0300

    Remove unused functions from functions.lua
    Removed implementations for Auto-Bunnyhop and ToggleChams functions.

--- functions.lua
+++ functions.lua
@@ -1,35 +1,33 @@
 -- functions.lua
 return function(Config)
     local Functions = {}
-    local Players = game:GetService("Players")
-    local LocalPlayer = Players.LocalPlayer
-
-    -- Реализация Auto-Bunnyhop
-    Functions.AutoBunnyhop = function()
-        local character = LocalPlayer.Character
-        if character and character:FindFirstChild("Humanoid") then
-            if LocalPlayer:GetMouse().Button1Down then -- Пример условия
-                character.Humanoid.Jump = true
-            end
-        end
-    end
-
-    -- Реализация Chams (простой вариант - Highlight)
-    Functions.ToggleChams = function(state)
-        for _, player in pairs(Players:GetPlayers()) do
-            if player ~= LocalPlayer and player.Character then
-                local highlight = player.Character:FindFirstChild("Highlight")
-                if state then
-                    if not highlight then
-                        highlight = Instance.new("Highlight", player.Character)
-                        highlight.FillColor = Color3.fromRGB(255, 0, 0)
-                    end
-                else
-                    if highlight then highlight:Destroy() end
-                end
-            end
-        end
-    end
+    
+    -- Rage
+    Functions.RageBot = function() end
+    Functions.Aimbot = function() end
+    Functions.AutoShoot = function() end
+    Functions.Resolver = function() end
+    Functions.MinDamage = function() end
+    
+    -- Anti-Aim
+    Functions.AntiAim = function() end
+    Functions.Fakeduck = function() end
+    
+    -- Legit
+    Functions.LegitAim = function() end
+    Functions.Triggerbot = function() end
+    Functions.RCS = function() end
+    
+    -- Visuals
+    Functions.Chams = function() end
+    Functions.Skeletons = function() end
+    Functions.InfoESP = function() end
+    
+    -- Inventory
+    Functions.Skinchanger = function() end
+    
+    -- Misc
+    Functions.Movement = function() end
 
     return Functions
 end
