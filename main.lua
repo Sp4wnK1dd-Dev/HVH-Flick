9639a1e1d2ad637f8d904ffcef008b8a434eaa90
Jason DEV <yarkisrngr@gmail.com>
2026-06-27 17:14:24 +0300

    Add UI menu creation to main.lua

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ local BaseUrl = "https://raw.githubusercontent.com/Sp4wnK1dd-Dev/HVH-Flick/main/
 
 local Config = loadstring(game:HttpGet(BaseUrl .. "config.lua"))()
 local Functions = loadstring(game:HttpGet(BaseUrl .. "functions.lua"))()(Config)
+local UI = Menu.CreateMenu() -- или просто Menu()
 
 game:GetService("RunService").RenderStepped:Connect(function()
     -- Bunnyhop
