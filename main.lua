8e1dbb76ca7376d38a9aa3c8b71fb85a9f470668
Jason DEV <yarkisrngr@gmail.com>
2026-06-27 17:01:16 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,15 @@
 -- main.lua
 local BaseUrl = "https://raw.githubusercontent.com/Sp4wnK1dd-Dev/HVH-Flick/main/"
 
--- Загружаем конфиг первым!
 local Config = loadstring(game:HttpGet(BaseUrl .. "config.lua"))()
-
--- Передаем Config в меню и функции
-local Menu = loadstring(game:HttpGet(BaseUrl .. "menu.lua"))()(Config) 
 local Functions = loadstring(game:HttpGet(BaseUrl .. "functions.lua"))()(Config)
 
--- Теперь цикл будет проверять Config.Toggles
 game:GetService("RunService").RenderStepped:Connect(function()
+    -- Bunnyhop
     if Config.Toggles["AutoBunnyhop"] then
-        Functions.Movement.AutoBunnyhop()
+        Functions.AutoBunnyhop()
     end
+
+    -- Chams (вызываем один раз при изменении или в цикле)
+    Functions.ToggleChams(Config.Toggles["Chams"])
 end)
