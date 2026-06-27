9ff7f4c37a96483d972112d0584aa3f4019a3c17
Jason DEV <yarkisrngr@gmail.com>
2026-06-27 16:58:38 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,17 @@
+-- config.lua
+local Config = {
+    -- Состояния функций
+    Toggles = {
+        ["AutoBunnyhop"] = false,
+        ["Triggerbot"] = false,
+        ["Aimbot"] = false,
+        ["Chams"] = false
+    },
+    -- Параметры
+    Settings = {
+        ["AimSmoothness"] = 0.5,
+        ["MinDamage"] = 20
+    }
+}
+
+return Config
